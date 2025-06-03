f428667b60a3a084af36d965201a9a06d9d591da
carlosalexandreoliveiraalves <carlosalexandreoliveiraalves3@gmail.com>
2025-06-03 14:24:29 -0300

    feat: correcao

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -7,8 +7,8 @@ export class TwilioService {
   private client: Twilio.Twilio;
 
   constructor() {
-    const accountSid = ''; // Substitua pelo seu Account SID
-    const authToken = ''; // Substitua pelo seu Auth Token
+    const accountSid = 'ACead3c002e91a5622f45f438c03748d6c'; // Substitua pelo seu Account SID
+    const authToken = '8fae4233f608e0cc9b77169ad42580aa';
     this.client = Twilio(accountSid, authToken);
   }
 
@@ -16,8 +16,8 @@ export class TwilioService {
   async sendSms(to: string, body: string): Promise<string> {
     const message = await this.client.messages.create({
       body,
-      to: `whatsapp:${to}`, // <- prefixo whatsapp
-      from: 'whatsapp:+19062545363', // <- número do sandbox
+      to: `${to}`, // <- prefixo whatsapp
+      from: '+19062545363', // <- número do sandbox
     });
 
     return message.sid;
