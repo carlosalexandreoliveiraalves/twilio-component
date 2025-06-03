c570300ba5a6182fbcef1a428362cf9efe37bcc4
carlosalexandreoliveiraalves <carlosalexandreoliveiraalves3@gmail.com>
2025-06-03 15:22:23 -0300

    feat: zap zap funfando

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -1,14 +1,17 @@
 // src/twilio/twilio.service.ts
 import { Injectable } from '@nestjs/common';
 import * as Twilio from 'twilio';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class TwilioService {
   private client: Twilio.Twilio;
 
   constructor() {
-    const accountSid = 'ACead3c002e91a5622f45f438c03748d6c'; // Substitua pelo seu Account SID
-    const authToken = '8fae4233f608e0cc9b77169ad42580aa';
+    const accountSid = process.env.accountSid; // Substitua pelo seu Account SID
+    const authToken = process.env.authToken; // Substitua pelo seu Auth Token
     this.client = Twilio(accountSid, authToken);
   }
 
@@ -22,5 +25,28 @@ export class TwilioService {
 
     return message.sid;
   }
+
+  async sendWhatsApp(to: string, body: string): Promise<string> {
+    const message = await this.client.messages.create({
+      body,
+      to: `${to}`, // <- prefixo whatsapp
+      from: '+19062545363', // <- número do sandbox
+    });
+
+    return message.sid;
+  }
+
+  async createMessage(to: string, variables: any) {
+    const message = await this.client.messages.create({
+      contentSid: process.env.contentSid,
+      contentVariables: JSON.stringify(variables),
+      from: "whatsapp:+19062545363",
+      messagingServiceSid: process.env.messagingServiceSid,
+      to: `whatsapp:${to}`,
+    });
+
+    return message.sid;
+  }
+
 }
 
